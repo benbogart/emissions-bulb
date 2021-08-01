26a39162d16147ffc8306a3cc0e3e5b44e07d75f
Ben Bogart <ben@benbogart.com>
2021-07-31 22:57:28 -0400

    lint

--- gridbulb.py
+++ gridbulb.py
@@ -6,8 +6,8 @@ from requests.auth import HTTPBasicAuth
 from time import sleep
 
 
-SCALING_FACTOR = 255/100 # adjust to limit color range
-UPDATE_FREQUENCY = 300 # seconds between updates
+SCALING_FACTOR = 255/100  # adjust to limit color range
+UPDATE_FREQUENCY = 300  # seconds between updates
 
 LATITUDE = [YOUR LATITUDE]
 LONGITUDE = [YOUR LONGITUDE]
@@ -15,15 +15,16 @@ LONGITUDE = [YOUR LONGITUDE]
 WATTTIME_USERNAME = '[YOUR WATTTIME USERNAME]'
 WATTTIME_PASSWORD = '[YOUR WATTTIME PASSWORD]'
 
-BULB_DEVICE_ID = '[YOUR BULBS DEVICE ID]' # string
-BULB_ADDRESS = '[YOUR BULBS IP ADDRESS]' # string
-BULB_LOCAL_KEY = '[YOUR BULBS LOCAL KEY]' # string
-BULB_VERSION = [YOUR BULBS VERSION] # as float (without '')
+BULB_DEVICE_ID = '[YOUR BULBS DEVICE ID]'  # string
+BULB_ADDRESS = '[YOUR BULBS IP ADDRESS]'  # string
+BULB_LOCAL_KEY = '[YOUR BULBS LOCAL KEY]'  # string
+BULB_VERSION = [YOUR BULBS VERSION]  # as float (without '')
 
 
 def get_bulb():
     bulb = tinytuya.BulbDevice(BULB_DEVICE_ID, BULB_ADDRESS, BULB_LOCAL_KEY)
     bulb.set_version(3.3)
+
     return bulb
 
 
@@ -37,28 +38,30 @@ def get_watttime_token():
 
 
 def get_watttime_pct():
-    
     token = get_watttime_token()
     headers = {'Authorization': 'Bearer {}'.format(token)}
     index_url = 'https://api2.watttime.org/index'
-    params = {'latitude': LATITUDE, 
-              'longitude': LONGITUDE,}
-    
-    rsp=requests.get(index_url, headers=headers, params=params)
+    params = {'latitude': LATITUDE,
+              'longitude': LONGITUDE}
+
+    rsp = requests.get(index_url, headers=headers, params=params)
     percent = json.loads(rsp.text)['percent']
+
     return int(percent)
+
 
 def calculate_rgb(watttime_pct):
     r = watttime_pct * SCALING_FACTOR
     g = 255 - r
     b = 0
-    
-    return r,g,b
+
+    return r, g, b
+
 
 # repeat forever
 while True:
     bulb = get_bulb()
     watttime_pct = get_watttime_pct()
-    r,g,b = calculate_rgb(watttime_pct)
-    bulb.set_colour(r,g,b)
+    r, g, b = calculate_rgb(watttime_pct)
+    bulb.set_colour(r, g, b)
     sleep(UPDATE_FREQUENCY)
